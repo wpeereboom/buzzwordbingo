ed6c66634fed548c73c8bfe09239ace34859809b
Winfred Peereboom <wpeereboom@developmentit.com>
2014-06-29 16:49:18 +0200

    Fixed the use statement for resultlist and retriever

--- buzzwordbingo.php
+++ buzzwordbingo.php
@@ -5,8 +5,8 @@ require_once __DIR__ . '/vendor/autoload.php';
 use Vnu\BuzzwordBingo;
 use Vnu\FileParser\BuzzWordFileParser;
 use Vnu\FileParser\UrlFileParser;
-use Vnu\JobPostingRetriever;
-use Vnu\ScoreList;
+use Vnu\JobPosting\Retriever;
+use Vnu\Score\ResultList;
 
 if(!isset($argv[1]) || !isset($argv[2]) ) {
     echo "No buzzword file or job url list given.";
@@ -22,8 +22,8 @@ $buzzWordFileParser->setFileContent(file_get_contents($argv[1]));
 $urlFileParser = new UrlFileParser();
 $urlFileParser->setFileContent(file_get_contents($argv[2]));
 
-$scoreList = new ScoreList();
-$jobPostingParser = new JobPostingRetriever();
+$scoreList = new ResultList();
+$jobPostingParser = new Retriever();
 
 $bingo = new BuzzwordBingo($buzzWordFileParser, $urlFileParser, $jobPostingParser, $scoreList);
 $result = $bingo->run();
